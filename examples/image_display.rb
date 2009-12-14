85f3ad136a885e34cc6c95ef18bd6f1792a33f39
Nicolas Martyanoff <khaelin@gmail.com>
2009-12-14 23:41:27 +0100

    add bindings for allegro_image

---
+++ examples/image_display.rb
@@ -0,0 +1,66 @@
+
+$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), "..", "lib")))
+
+require 'allegro'
+
+include Allegro
+
+if ARGV.length != 1
+    puts "usage: $0 <image file>"
+    exit 1
+end
+img_file = ARGV[0]
+
+al_install_system nil
+al_init_image_addon
+
+dpy = al_create_display 800, 600
+al_install_keyboard
+al_install_mouse
+
+al_set_window_title "ffi-allegro example: image display"
+
+evqueue = al_create_event_queue()
+al_register_event_source evqueue, al_get_display_event_source(dpy)
+al_register_event_source evqueue, al_get_keyboard_event_source
+al_register_event_source evqueue, al_get_mouse_event_source
+
+img = al_load_bitmap img_file
+if img.null?
+    puts "cannot load image from file #{img_file} (#{al_get_errno})"
+    exit 1
+end
+
+al_resize_display al_get_bitmap_width(img), al_get_bitmap_height(img)
+
+bgcolor = al_map_rgb 20, 20, 20
+
+stop = false
+while !stop do
+    al_clear_to_color bgcolor
+
+    al_draw_bitmap img, 0, 0, 0
+
+    al_flip_display
+
+    ev = Event.new
+    al_wait_for_event evqueue, ev
+
+    case ev[:type]
+    when ALLEGRO_EVENT_KEY_UP
+        if ev[:keyboard][:keycode] == ALLEGRO_KEY_ESCAPE
+            stop = true
+        end
+
+    when ALLEGRO_EVENT_DISPLAY_CLOSE
+        stop = true
+    end
+end
+
+al_destroy_bitmap img
+
+al_destroy_event_queue evqueue
+al_destroy_display dpy
+
+al_uninstall_system
+
---
+++ lib/allegro/ffi_allegro_image.rb
@@ -0,0 +1,48 @@
+
+require 'ffi'
+
+module Allegro extend FFI::Library
+    ffi_lib 'allegro_image-4.9.16'
+
+    ALLEGRO_IIO_LOADER_FUNCTION    = callback([:string], :pointer)
+    ALLEGRO_IIO_FS_LOADER_FUNCTION = callback([:pointer], :pointer)
+    ALLEGRO_IIO_SAVER_FUNCTION     = callback([:string, :pointer], :bool)
+    ALLEGRO_IIO_FS_SAVER_FUNCTION  = callback([:pointer, :pointer], :bool)
+
+    attach_function :al_init_image_addon, [], :bool
+    attach_function :al_shutdown_image_addon, [], :void
+    attach_function :al_register_bitmap_loader, [:string,
+                    ALLEGRO_IIO_LOADER_FUNCTION], :bool
+    attach_function :al_register_bitmap_saver, [:string,
+                    ALLEGRO_IIO_SAVER_FUNCTION], :bool
+    attach_function :al_register_bitmap_loader_stream, [:string,
+                    ALLEGRO_IIO_FS_LOADER_FUNCTION], :bool
+    attach_function :al_register_bitmap_saver_stream, [:string,
+                    ALLEGRO_IIO_FS_SAVER_FUNCTION], :bool
+    attach_function :al_load_bitmap, [:string], :pointer
+    attach_function :al_load_bitmap_stream, [:pointer, :string], :pointer
+    attach_function :al_save_bitmap, [:string, :pointer], :bool
+    attach_function :al_save_bitmap_stream, [:pointer, :string, :pointer],
+                    :bool
+    attach_function :al_get_allegro_image_version, [], :uint32
+    attach_function :al_load_pcx, [:string], :pointer
+    attach_function :al_load_bmp, [:string], :pointer
+    attach_function :al_load_tga, [:string], :pointer
+    attach_function :al_load_png, [:string], :pointer
+    attach_function :al_load_jpg, [:string], :pointer
+    attach_function :al_load_pcx_stream, [:pointer], :pointer
+    attach_function :al_load_bmp_stream, [:pointer], :pointer
+    attach_function :al_load_tga_stream, [:pointer], :pointer
+    attach_function :al_load_png_stream, [:pointer], :pointer
+    attach_function :al_load_jpg_stream, [:pointer], :pointer
+    attach_function :al_save_pcx, [:string, :pointer], :bool
+    attach_function :al_save_bmp, [:string, :pointer], :bool
+    attach_function :al_save_tga, [:string, :pointer], :bool
+    attach_function :al_save_png, [:string, :pointer], :bool
+    attach_function :al_save_jpg, [:string, :pointer], :bool
+    attach_function :al_save_pcx_stream, [:pointer, :pointer], :bool
+    attach_function :al_save_bmp_stream, [:pointer, :pointer], :bool
+    attach_function :al_save_tga_stream, [:pointer, :pointer], :bool
+    attach_function :al_save_png_stream, [:pointer, :pointer], :bool
+    attach_function :al_save_jpg_stream, [:pointer, :pointer], :bool
+end
