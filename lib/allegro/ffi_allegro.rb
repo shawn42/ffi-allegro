0ea7bbed1b5e8dcc009f46482c23b60ba8428f78
Nicolas Martyanoff <khaelin@gmail.com>
2009-12-15 23:17:45 +0100

    add bindings for state functions

--- lib/allegro/ffi_allegro.rb
+++ lib/allegro/ffi_allegro.rb
@@ -127,6 +127,26 @@ module Allegro extend FFI::Library
     attach_function :al_utf16_encode, [:pointer, :int32], :uint
     #}}}
 
+    #{{{1 State
+    ALLEGRO_STATE_NEW_DISPLAY_PARAMETERS = 0x0001
+    ALLEGRO_STATE_NEW_BITMAP_PARAMETERS  = 0x0002
+    ALLEGRO_STATE_DISPLAY                = 0x0004
+    ALLEGRO_STATE_TARGET_BITMAP          = 0x0008
+    ALLEGRO_STATE_BLENDER                = 0x0010
+    ALLEGRO_STATE_NEW_FILE_INTERFACE     = 0x0020
+    ALLEGRO_STATE_ALL                    = 0xffff
+    ALLEGRO_STATE_BITMAP                 = ALLEGRO_STATE_TARGET_BITMAP
+                                         + ALLEGRO_STATE_NEW_BITMAP_PARAMETERS
+
+    class ALLEGRO_STATE < FFI::Struct
+        layout :_tls, [:char, 256],
+               :flags, :int
+    end
+
+    attach_function :al_store_state, [:pointer, :int], :void
+    attach_function :al_restore_state, [:pointer], :void
+    #}}}
+
     #{{{1 Paths
     attach_function :al_create_path, [:string], :pointer
     attach_function :al_create_path_for_directory, [:string], :pointer
